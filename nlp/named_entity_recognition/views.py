dbb23863e37272629452515222bf285f1555f7e4
Daniel, Kim Hangon <db52683@um.edu.mo>
2022-04-12 11:21:18 +0800

    write class view

--- nlp/named_entity_recognition/views.py
+++ nlp/named_entity_recognition/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
+class NERView(View):
+    def get(self, request):
+        return render(request, "named_entity_recognition/form.html")
 
+    def post(self, request):
+        input_text = request.POST['textarea']
+        return render(request, "named_entity_recognition/ner_result.html")
 # Create your views here.
 def index(request):
     return render(request, "named_entity_recognition/form.html")
